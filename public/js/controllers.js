b828de35422f417225c2fbefe5c42f912721a67f
Hack Reactor Students <students@hackreactor.com>
2013-12-13 19:55:15 -0800

    Search functionality working

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('ShortlyApp')
+var app = angular.module('ShortlyApp')
   .controller('MainController', function($scope, $http) {
 
     $http({
@@ -9,4 +9,36 @@ angular.module('ShortlyApp')
     });
 
     $scope.predicate = '-title';
+    $scope.filter = function() {
+
+    }
   });
+
+app.filter('searchFor', function(){
+
+  // All filters must return a function. The first parameter
+  // is the data that is to be filtered, and the second is an
+  // argument that may be passed with a colon (searchFor:searchString)
+
+  return function(arr, searchString){
+
+    if(!searchString){
+      return arr;
+    }
+
+    var result = [];
+
+    searchString = searchString.toLowerCase();
+
+    // Using the forEach helper method to loop through the array
+    angular.forEach(arr, function(item){
+
+      if(item.title.toLowerCase().indexOf(searchString) !== -1){
+        result.push(item);
+      }
+
+    });
+
+    return result;
+  };
+});
